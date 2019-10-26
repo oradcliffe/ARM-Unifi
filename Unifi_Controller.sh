54eab3bd88b231c7c5391261d81d43c77df72258
Oliver Radcliffe <ojradcliffe@gmail.com>
2019-10-26 05:44:53 -0500

    updating bash installer

--- Unifi_Controller.sh
+++ Unifi_Controller.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-echo 'deb http://www.ubnt.com/downloads/unifi/debian stable ubiquiti' | sudo tee -a /etc/apt/sources.list.d/100-ubnt.list
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv C0A52C50 && sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
-sudo apt-get update
-sudo apt-get install unifi -y
-
+sudo apt update && sudo apt install ca-certificates apt-transport-https -y
+echo 'deb http://www.ui.com/downloads/unifi/debian stable ubiquiti' | sudo tee /etc/apt/sources.list.d/100-ubnt-unifi.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+sudo wget -O /etc/apt/trusted.gpg.d/unifi-repo.gpg https://dl.ui.com/unifi/unifi-repo.gpg
+sudo apt update
+sudo apt install unifi -y
